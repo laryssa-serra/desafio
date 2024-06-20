a7fa984277b34c5287a6c8e0a259ec72041dbd3a
laryssa-serra <oliv.laryssa@gmail.com>
2024-06-20 08:28:04 -0300

    feat: add application starter

---
+++ src/main/kotlin/com/luizalabs/logistica/desafio/DesafioApplication.kt
@@ -0,0 +1,11 @@
+package com.luizalabs.logistica.desafio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioApplication>(*args)
+}
---
+++ src/test/kotlin/com/luizalabs/logistica/desafio/DesafioApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.luizalabs.logistica.desafio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioApplicationTests {
+	@Test
+	fun contextLoads() {
+	}
+}
